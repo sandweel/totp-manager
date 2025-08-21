095061532f0955e2d08bfb5ac2474243bee7b8c9
Vitalii <kalyuzhnyak@gmail.com>
2025-08-21 16:31:35 +0300

    Added / redirect to /totp/list

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def health_check():
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request, user: Optional[User] = Depends(get_current_user_if_exists)):
-    return templates.TemplateResponse("index.html", {"request": request, "user": user})
-
+    # return templates.TemplateResponse("index.html", {"request": request, "user": user})
+    return RedirectResponse(url="/totp/list")
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(settings.PORT or 8000), reload=True)
